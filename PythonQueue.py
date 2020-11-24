a3f751e67e2da1506303c6a6d4f1db803fa37da3
jenniatz <71726141+jenniatz@users.noreply.github.com>
2020-11-23 19:55:19 -0500

    Python Queue

---
+++ PythonQueue.py
@@ -0,0 +1,35 @@
+#enqueue
+names= []
+
+print ("names enqueued")
+names.append("sam")
+names.append('Justin')
+names.append("george")
+names.append("venus")
+names.append("Jack")
+names.append("joe")
+names.append("ron")
+names.append("mike")
+names.append("sally")
+names.append("maria")
+
+print(names)
+
+#dequeue
+print ("names dequeued")
+
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+
+
+
+
+
